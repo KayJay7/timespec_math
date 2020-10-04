0fc5ce430dea66b9cdf2ca6c56a5eda4bef8fd27
KayJay7 <alvisebruniera@gmail.com>
2020-10-04 21:33:18 +0200

    Fixed div

--- timespec_math.c
+++ timespec_math.c
@@ -32,12 +32,8 @@ void timespec_mul(const struct timespec *a, int b, struct timespec *result) {
 }
 
 //(a+b/1E9)/c = a/c+b/cE9
-//sometimes result->tv_nsec is 1 smaller than it should, still figuring out why
-//1.765305600/3 gives 0.588435199 instead of 0.588435200
 void timespec_div(const struct timespec *a, int b, struct timespec *result) {
-    long long temp = a->tv_sec % b; 
-    temp = (temp * 1000000000LL) / (long long) b;
-    temp = temp + ((long long) a->tv_nsec / (long long) b);
-    result->tv_nsec = temp % 1000000000LL;
+    long long temp = (((long long) (a->tv_sec % b) * 1000000000LL) + (long long) a->tv_nsec) / (long long) b;
     result->tv_sec = (a->tv_sec / b) + (temp / 1000000000LL);
+    result->tv_nsec = temp % 1000000000LL;
 }
