a4383f5f54086573d403ac64129a9c38790aabd5
KayJay7 <alvisebruniera@gmail.com>
2020-10-04 20:24:20 +0200

    Fixed mul and div

--- timespec_math.c
+++ timespec_math.c
@@ -1,15 +1,20 @@
 #include <time.h>
 
-void timespec_dif(struct timespec *a, struct timespec *b, struct timespec *result) {
+void timespec_dif(const struct timespec *a, const struct timespec *b, struct timespec *result);
+void timespec_sum(const struct timespec *a, const struct timespec *b, struct timespec *result);
+void timespec_mul(const struct timespec *a,  int b, struct timespec *result);
+void timespec_div(const struct timespec *a,  int b, struct timespec *result);
+
+void timespec_dif(const struct timespec *a, const struct timespec *b, struct timespec *result) {
     result->tv_sec = a->tv_sec - b->tv_sec;
     result->tv_nsec = a->tv_nsec - b->tv_nsec;
     if (result->tv_nsec < 0) {
-        result->tv_sec++;
+        --result->tv_sec;
         result->tv_nsec += 1000000000L;
     }
 }
 
-void timespec_sum(struct timespec *a, struct timespec *b, struct timespec *result) {
+void timespec_sum(const struct timespec *a, const struct timespec *b, struct timespec *result) {
     result->tv_sec  = a->tv_sec + b->tv_sec;
     result->tv_nsec = a->tv_nsec + b->tv_nsec;
     if (result->tv_nsec > 1000000000L) {
@@ -18,20 +23,21 @@ void timespec_sum(struct timespec *a, struct timespec *b, struct timespec *resul
     }
 }
 
-//(a+1/b)*c = c*a+c/b
-void timespec_mul(struct timespec *a, int b, struct timespec *result) {
-	long long temp = (long long) b * 1000000000LL;
-	temp = temp / result->tv_nsec;
+//(a+b/1E9)*c = c*a+cb/1E9
+void timespec_mul(const struct timespec *a, int b, struct timespec *result) {
+	long long temp = (long long) b * (long long) a->tv_nsec;
     result->tv_sec = a->tv_sec * b;
     result->tv_sec = result->tv_sec + (temp / 1000000000LL);
     result->tv_nsec = temp % 1000000000LL;
 }
 
-//(a+1/b)/c = a/c+1/(b*c)
-void timespec_div(struct timespec *a, int b, struct timespec *result) {
-    long long temp = a->tv_sec % b;
+//(a+b/1E9)/c = a/c+b/cE9
+//sometimes result->tv_nsec is 1 smaller than it should, still figuring out why
+//1.765305600/3 gives 0.588435199 instead of 0.588435200
+void timespec_div(const struct timespec *a, int b, struct timespec *result) {
+    long long temp = a->tv_sec % b; 
     temp = (temp * 1000000000LL) / (long long) b;
-    temp = temp + ((long long) a->tv_nsec * (long long) b);
+    temp = temp + ((long long) a->tv_nsec / (long long) b);
     result->tv_nsec = temp % 1000000000LL;
-    result->tv_sec = result->tv_sec + (temp / 1000000000LL);
+    result->tv_sec = (a->tv_sec / b) + (temp / 1000000000LL);
 }
