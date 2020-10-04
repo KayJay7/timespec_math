c2566ef16bcbdf6f35f941d5e6eeaae5d7cd9792
KayJay7 <alvisebruniera@gmail.com>
2020-10-04 21:45:59 +0200

    Minor change to dif

--- timespec_math.c
+++ timespec_math.c
@@ -9,7 +9,7 @@ void timespec_dif(const struct timespec *a, const struct timespec *b, struct tim
     result->tv_sec = a->tv_sec - b->tv_sec;
     result->tv_nsec = a->tv_nsec - b->tv_nsec;
     if (result->tv_nsec < 0) {
-        --result->tv_sec;
+        result->tv_sec--;
         result->tv_nsec += 1000000000L;
     }
 }
